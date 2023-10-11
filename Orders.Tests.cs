b3572ac8a035728b514884ed91578920754a715c
Sergio Azcona <SergioAzcona@odysseylogistics.com>
2023-10-11 10:37:13 -0400

    initial commit

--- Orders.Tests.cs
+++ Orders.Tests.cs
@@ -12,46 +12,46 @@ namespace ConsoleAppPlayground.Tests
             Orders order = new(9, 10);
             Orders order1 = new(11, 12);
 
-            Orders order3 = new(9, 13);
+            //Orders order3 = new(9, 13); cannot try to actually add an instance
             var exception3 = Assert.Throws<ArgumentException>(() => order3);
-            Assert.Equal("9 is not a unique OrderID.", exception3.Message); // Should throw due to non-unique OrderID
+            Assert.Equal("9 is not unique.", Assert.Throws<ArgumentException>(() => new Orders(9, 13)).Message); // Should throw due to non-unique OrderID
 
         }
 
 
-        //[Fact]
-        //public void AddItemToDictionaries_CanAddItemsToDict()
-        //{
-        //    Orders order = new(1, 2);
-        //    Orders order1 = new(3, 4);
+        [Fact]
+        public void AddItemToDictionaries_CanAddItemsToDict()
+        {
+            Orders order = new(1, 2);
+            Orders order1 = new(3, 4);
 
-        //    Assert.Empty(order.ItemInfoDict);
-        //    Assert.Empty(order1.ItemInfoDict);
+            Assert.Empty(order.ItemInfoDict);
+            Assert.Empty(order1.ItemInfoDict);
 
-        //    Orders.AddItemToDictionaries(order.ItemInfoDict, order.ItemID);
+            Orders.AddItemToDictionaries(order.ItemInfoDict, order.ItemID);
 
-        //    Assert.Contains(order.ItemID, order.ItemInfoDict.Keys);
-        //    Assert.DoesNotContain(order1.ItemID, order1.ItemInfoDict.Keys);
+            Assert.Contains(order.ItemID, order.ItemInfoDict.Keys);
+            Assert.DoesNotContain(order1.ItemID, order1.ItemInfoDict.Keys);
 
-        //    Assert.Single(order.ItemInfoDict.Keys);
-        //    Assert.Empty(order1.ItemInfoDict);
-        //}
+            Assert.Single(order.ItemInfoDict.Keys);
+            Assert.Empty(order1.ItemInfoDict);
+        }
 
-        //[Fact]
-        //public void AddItemToDictionaries_AddsOnlyValidItems()
-        //{
-        //    Orders order = new(5, 6);
-        //    Orders order1 = new(7, 8);
+        [Fact]
+        public void AddItemToDictionaries_AddsOnlyValidItems()
+        {
+            Orders order = new(5, 6);
+            Orders order1 = new(7, 8);
 
-        //    Assert.Empty(order.ItemInfoDict);
-        //    Assert.Empty(order1.ItemInfoDict);
+            Assert.Empty(order.ItemInfoDict);
+            Assert.Empty(order1.ItemInfoDict);
 
-        //    Orders.AddItemToDictionaries(order.ItemInfoDict, order.OrderID);
-        //    Orders.AddItemToDictionaries(order1.ItemInfoDict, order1.ItemID);
+            Orders.AddItemToDictionaries(order.ItemInfoDict, order.OrderID);
+            Orders.AddItemToDictionaries(order1.ItemInfoDict, order1.ItemID);
 
-        //    Assert.Empty(order.ItemInfoDict);
-        //    Assert.Single(order1.ItemInfoDict);
-        //}
+            Assert.Empty(order.ItemInfoDict);
+            Assert.Single(order1.ItemInfoDict);
+        }
     }
 
 }
